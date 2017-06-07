23da0cbdcca99bcb5c8d001faaa26f4558c7101c
Jefferson John <jefferson.john.rego@gmail.com>
2017-06-07 17:01:23 -0300

    Retirada do ESP

--- Main/Main.ino
+++ Main/Main.ino
@@ -2,90 +2,73 @@
 #include <SPI.h>
 #include <Wire.h>
 #include "RTClib.h"
-#include "SoftwareSerial.h"
 
 File arquivo;
 RTC_DS1307 rtc;
-SoftwareSerial esp8266(2, 3);
 
-String enviaEsp(String command, const int timeout);
-void enviaWeb(String msg);
-void limpa();
+byte pinopir = 8;
+boolean ligado = false;
+
+void apaga();
 void grava();
+void leitura();
 
-byte pir = 8;
-
-void setup(){
-  rtc.begin();
-  esp8266.begin(19200);
+void setup() {
   Serial.begin(9600);
+  rtc.begin();
+  pinMode(pinopir, INPUT);
   if (!SD.begin(9)) {
-    Serial.println("Erro no cartao");
+    Serial.println("Falha no Cartao");
+    return;
   }
-  pinMode(pir, INPUT);
-  /* Para se conectar consultar o arquivo de conexão */
-  enviaEsp("AT+CIPMUX=1\r\n", 1000);
-  enviaEsp("AT+CIPSERVER=1,80\r\n", 1000);
+  arquivo = SD.open("pir.txt", FILE_WRITE);
+  if (arquivo) {
+    Serial.println("Arquivo OK");
+  }
+  else{
+    Serial.println("Erro no Arquivo");
+  }
+  arquivo.close();
 }
 
-void loop(){
-  if (digitalRead(pir)){
-    Serial.println("Movimento Detectado");
-    grava ();
-    delay (4000);
-  }
-  else if (esp8266.available()){
-    if (esp8266.find("+IPD,")){
-      delay(100);
-      if(esp8266.find("limpa")){
-        limpa();
-        enviaWeb("O Arquivo sera limpo.");
-      }
-      else{
-        arquivo = SD.open("pir.txt");
-        if (arquivo){
-          String linha;
-          while (arquivo.available()){
-            char ch = arquivo.read();
-            if (ch == '\n'){
-              enviaWeb(linha);
-              linha = "";
-            }
-            else{
-              linha += ch;
-            }
-          }
-          arquivo.close();
-          enviaWeb("Fim");
-        }
-        else {
-          enviaWeb("Erro ao acessar o cartao");
-        }
-      }
-    }   
-  }
-}
-
-void enviaWeb(String msg){
-  msg += "\r\n";
-  String cipSend = "AT+CIPSEND=0,";
-  cipSend += msg.length();
-  cipSend += "\r\n";
-  enviaEsp(cipSend, 1000);
-  enviaEsp(msg, 1000);
-}
-
-String enviaEsp(String command, const int timeout){
-  String response = "";
-  esp8266.print(command);
-  long int time = millis();
-  while ((time + timeout) > millis()){
-    while (esp8266.available()){
-      char c = esp8266.read();
-      response += c;
+void loop() {
+  if (Serial.available() > 0){
+    byte e = Serial.read();
+    if (e == 'l'){
+      ligado = true;
+      Serial.println("Sistema Ligado");
+    }
+    else if (e == 'r'){
+      Serial.println("Lendo pir");
+      leitura ();
+    }
+    else if (e == 'd'){
+      Serial.println("Deletando arquivo");
+      apaga ();
+    }
+    else if (e == 'g'){
+      Serial.println("Testando gravacao");
+      grava ();
+    }
+    else if (e == 'h'){
+      ligado = false;
+      Serial.println("Sistema Desligado");
+    }
+    else{
+      Serial.println("l para ligaro listema");
+      Serial.println("r para ler");
+      Serial.println("d para apagar arquivo");
+      Serial.println("h para desligar o sistema");
     }
   }
-  Serial.print(response);
-  return response;
+  if(ligado){
+    if (digitalRead(pinopir)){  //Com movimento
+      Serial.println("Movimento Detectado");
+      grava ();
+      delay (4000);
+    }
+  }
 }
+
+
 
--- Main/SD.ino
+++ Main/SD.ino
@@ -1,54 +1,77 @@
-void limpa(){
-  SD.remove ("pir.txt");
+void leitura (){
+  arquivo = SD.open("pir.txt");
+  if (arquivo){
+    while (arquivo.available()){
+      Serial.write(arquivo.read());
+    }
+  arquivo.close();
+  Serial.println("Termino da leitura");
+  }
+  else{
+    Serial.println("Erro na leitura");
+  }
 }
 
-void grava(){
+void apaga (){
+  SD.remove ("pir.txt");
+  if (!SD.exists ("pir.txt") ){
+    Serial.println ("Arquivo apagado com sucesso");
+  }
+}
+
+void grava (){
   arquivo = SD.open("pir.txt", FILE_WRITE);
   if (arquivo) {
+    Serial.println("Iniciando gravacao");
     DateTime now = rtc.now();
     arquivo.print("Movimento: ");
     if(now.hour() > 9){
-      arquivo.print(now.hour());
+      arquivo.print(now.hour(), DEC);
     }
     else{
-      arquivo.print('0');
-      arquivo.print(now.hour());
+      arquivo.print("0");
+      arquivo.print(now.hour(), DEC);
     }
     arquivo.print(':');
     if(now.minute() > 9){
-      arquivo.print(now.minute());
+      arquivo.print(now.minute(), DEC);
     }
     else{
-      arquivo.print('0');
-      arquivo.print(now.minute());
+      arquivo.print("0");
+      arquivo.print(now.minute(), DEC);
     }
     arquivo.print(':');
     if(now.second() > 9){
-      arquivo.print(now.second());
+      arquivo.print(now.second(), DEC);
     }
     else{
-      arquivo.print('0');
-      arquivo.print(now.second());
+      arquivo.print("0");
+      arquivo.print(now.second(), DEC);
     }
     arquivo.print(' ');
     if(now.day() > 9){
-      arquivo.print(now.day());
+      arquivo.print(now.day(), DEC);
     }
     else{
-      arquivo.print('0');
-      arquivo.print(now.day());
+      arquivo.print("0");
+      arquivo.print(now.day(), DEC);
     }
     arquivo.print('/');
     if(now.month() > 9){
-      arquivo.print(now.month());
+      arquivo.print(now.month(), DEC);
     }
     else{
-      arquivo.print('0');
-      arquivo.print(now.month());
+      arquivo.print("0");
+      arquivo.print(now.month(), DEC);
     }
     arquivo.print('/');
-    arquivo.print(now.year());
+    arquivo.print(now.year(), DEC);
     arquivo.println();
     arquivo.close();  
+    Serial.println("Fim da gravacao");
+  }
+  else{
+    Serial.println("Erro na gravacao");
   }
 }
+
